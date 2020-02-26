c7f2936b853c25551800d5addff6d5e78772c895
YellowTulipShow <1426689530@qq.com>
2020-02-26 23:40:07 +0800

    修改内容

--- main2.py
+++ main2.py
@@ -8,6 +8,9 @@ import convert
 import font_format
 
 def main(config):
+    print(font_format.interval_line())
+    print("Start find need git operating repositories:")
+
     git_project_paths = []
     global_ignores = config.get('ignores', [])
     roots = config.get('roots', [])
@@ -20,17 +23,12 @@ def main(config):
         son_paths = subproject_address_list(root_path, ignores=ignores)
         git_project_paths.extend(son_paths)
 
-    is_have_error_git_repo = False
-    for path in git_project_paths:
-        is_exception, rcode, message_git_status = check_git_project(path)
-        if is_exception:
-            show_git_repo_result(path, rcode, message_git_status)
-            is_have_error_git_repo = True
-
-    if not is_have_error_git_repo:
-        print(font_format.interval_line())
-        print(font_format.font_green("All warehouses are very clean... ok!"))
-        print(font_format.interval_line())
+        check_repo_status = git_status(son_paths);
+        if not check_repo_status.execute_results():
+            print(font_format.interval_line())
+            print("root: {}".format(root_path));
+        for msg in check_repo_status.results:
+            print(msg)
 
 def subproject_address_list(root, ignores=[]):
     def is_ignore(folder):
@@ -55,52 +53,70 @@ def subproject_address_list(root, ignores=[]):
         results.extend(r)
     return results
 
-def check_git_project(path):
-    os.chdir(path)
-    message_git_status = execute_command('git status')
-    ris, rcode = is_need_hint_status_message(message_git_status)
-    if ris and rcode != "":
-        return True, rcode, message_git_status
-    return False, '', ''
-
 def execute_command(command_string):
     with os.popen(r'git status', 'r') as f:
         text = f.read()
     return text
 
-def is_need_hint_status_message(msg):
-    yes = [
-        "Changes not staged for commit:",
-        "Changes to be committed:",
-        "(use \"git push\" to publish your local commits)",
-        "Untracked files:"
-    ]
-    for code in yes:
-        if code in msg:
-            return True, code
-    no = [
-        "nothing to commit, working tree clean"
-    ]
-    for code in no:
-        if code in msg:
-            return False, code
-    return False, ""
+class git_status():
+    def __init__(self, git_project_paths):
+        self.git_project_paths = git_project_paths
+        self.results = []
+    def execute_results(self):
+        is_have_error_git_repo = False
+        for path in self.git_project_paths:
+            is_exception, rcode, message_git_status = self.check_git_project(path)
+            if is_exception:
+                self.show_git_repo_result(path, rcode, message_git_status)
+                is_have_error_git_repo = True
+        if not is_have_error_git_repo:
+            self.results.append(font_format.interval_line())
+            self.results.append(font_format.font_green("All warehouses are very clean... ok!"))
+            self.results.append(font_format.interval_line())
+            return False
+        return True
 
-def show_git_repo_result(path, rcode, message_git_status):
-    if convert.is_window_system() and convert.is_window_path(path):
-        path = convert.to_linux_path(path)
+    def check_git_project(self, path):
+        os.chdir(path)
+        message_git_status = execute_command('git status')
+        ris, rcode = self.is_need_hint_status_message(message_git_status)
+        if ris and rcode != "":
+            return True, rcode, message_git_status
+        return False, '', ''
 
-    rcode_format = font_format.font_fuchsia(rcode)
-    rcode_format = message_git_status.replace(rcode, rcode_format)
-    rcode_format = rcode_format.strip('\n')
+    def is_need_hint_status_message(self, msg):
+        yes = [
+            "Changes not staged for commit:",
+            "Changes to be committed:",
+            "(use \"git push\" to publish your local commits)",
+            "Untracked files:"
+        ]
+        for code in yes:
+            if code in msg:
+                return True, code
+        no = [
+            "nothing to commit, working tree clean"
+        ]
+        for code in no:
+            if code in msg:
+                return False, code
+        return False, ""
 
-    red_url = path.strip('\n')
-    red_url = font_format.font_red(red_url)
+    def show_git_repo_result(self, path, rcode, message_git_status):
+        if convert.is_window_system() and convert.is_window_path(path):
+            path = convert.to_linux_path(path)
 
-    print(font_format.interval_line())
-    print("path: {}".format(red_url))
-    print("out:\n{}".format(rcode_format))
-    print(font_format.interval_line())
+        rcode_format = font_format.font_fuchsia(rcode)
+        rcode_format = message_git_status.replace(rcode, rcode_format)
+        rcode_format = rcode_format.strip('\n')
+
+        red_url = path.strip('\n')
+        red_url = font_format.font_red(red_url)
+
+        self.results.append(font_format.interval_line())
+        self.results.append("path: {}".format(red_url))
+        self.results.append("out:\n{}".format(rcode_format))
+        self.results.append(font_format.interval_line())
 
 if __name__ == '__main__':
     # 读取 json 配置文件
