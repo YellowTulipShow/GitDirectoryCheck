024b3e06664dbce9a0843af6d523369bf6838218
YellowTulipShow <main@yellowtulipshow.site>
2020-05-06 17:35:02 +0800

    add ITask.py file

---
+++ ITask.py
@@ -0,0 +1,31 @@
+# coding: UTF-8
+
+# interface接口: 任务接口
+class ITask():
+    def __init__(self):
+        self.repo = {}
+        self.msgs = {}
+
+    """
+    注入用户参数内容
+    Returns:
+        ArgumentParser 回传传入的解析器对象
+    """
+    def UserArgumentAdd(self, keyname, parser):
+        return parser
+
+    """
+    执行相关的任务操作
+    Returns:
+        repo 回传传入的仓库相关对象
+    """
+    def OnExecute(self, repo):
+        return repo
+
+    """
+    输出打印结果
+    Returns:
+        string[] 打印结果
+    """
+    def PrintResult(self, repo):
+        return self.msgs
