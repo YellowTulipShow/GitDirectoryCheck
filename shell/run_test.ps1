bdc43919f76568c6cbb778cabe7f227526d4c229
YellowTulipShow <main@yellowtulipshow.site>
2022-06-17 16:00:22 +0800

    Sync YTS.Git project

--- shell/run_test.ps1
+++ shell/run_test.ps1
@@ -2,7 +2,7 @@ $ExecutePath = $PWD
 Set-Location $PSScriptRoot
 Set-Location ..
 
-dotnet run --project ./src/RunCommand/RunCommand.csproj
+dotnet run --project ./src/GitCheckCommand/GitCheckCommand.csproj
 
 Set-Location $ExecutePath
 if ($PSScriptRoot -eq $ExecutePath) {
